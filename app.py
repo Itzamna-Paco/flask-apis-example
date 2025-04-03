819b0be5b007fed66a0a6abcd1ecc0493854fc51
smash-datasci <vivertido@proton.me>
2025-04-03 22:23:31 +0000

    basic app 2 route

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+#below app declaration
+@app.route("/about")
+def about():
+    return render_template("about.html")
